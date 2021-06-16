3a07bb3cdb2be24b26bb1b0969e3d24d74e2ef9a
AnanthShen <72775028+AnanthShen@users.noreply.github.com>
2021-06-16 16:38:12 +0530

    Setting up Heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 console.log("Starting the Server");
 
@@ -7,4 +8,4 @@ app.get('', (req, res) => {
     res.send("Hello!!");
 })
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
